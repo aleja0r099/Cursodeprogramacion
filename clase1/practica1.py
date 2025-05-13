50f78ef8de88434fc448ac80a4ccf6356c72e709
aleja0r099 <rodriguezcorzoalejandra@gmail.com>
2025-05-13 12:01:25 -0400

    name

--- clase1/practica1.py
+++ clase1/practica1.py
@@ -1,1 +1,15 @@
-print("HELLO WORLD")
+import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+clear_screen()
+
+print ("-------------------")
+print ("---------------")  
+
+USER_NAME=input("WHAT'S YOUR NAME? ")
+print(f"HELLO {USER_NAME}!")
+USER_LASTNAME=input("WHAT'S YOUR LAST NAME? ")
+print(f"HELLO {USER_NAME} {USER_LASTNAME}!")
+
